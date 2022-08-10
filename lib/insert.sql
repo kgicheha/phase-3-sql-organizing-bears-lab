87944ebf863835b5ade3f965deafc37cda3d876a
Kevin Gicheha <gichehakevin@gmail.com>
2022-08-10 14:34:46 -0700

    commit tree 3dcf397c084eb450250c7453ffe90a8f6830917f

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate",5,"M", "black", "good", 0);
+INSERT INTO bears VALUES (2, "Rowdy",6,"M","brown", "bad",  1);
+INSERT INTO bears VALUES (3, "Tabitha",7,"M", "brown", "good",  1);
+INSERT INTO bears VALUES (4, "Sergeant Brown",4,"M","white", "bad", 1);
+INSERT INTO bears VALUES (5, "Melissa",5,"M","black", "good", 1);
+INSERT INTO bears VALUES (6, "Grinch",10,"F", "white", "good", 1);
+INSERT INTO bears VALUES (7, "Wendy",5,"F", "black","good", 1);
+INSERT INTO bears VALUES (8, null,5,"F", "black", "good", 0);
